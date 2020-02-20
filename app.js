7c9dcc29ab61d2a6f82e739237485e580096add3
Nikola <nikoladmit21@gmail.com>
2020-02-20 13:26:31 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express     = require('express');
+const app         = express();
+const path        = require('path');
+const bodyParser  = require('body-parser');
+const urlShortener = require('node-url-shortener');
+
+
+const port        = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.post('/url', function(req, res) {
+    const url = req.body.url;
+
+    urlShortener.short(url, function(err, shortUrl){
+        res.send(shortUrl);
+    });
+});
+
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
