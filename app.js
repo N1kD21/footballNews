26fbe4e3fee059fa2acd35a608a46ae4f75f941c
Nikola <nikoladmit21@gmail.com>
2020-02-20 13:32:17 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express     = require('express');
-const app         = express();
-const path        = require('path');
-const bodyParser  = require('body-parser');
-const urlShortener = require('node-url-shortener');
+const express       = require('express');
+const app           = express();
+const path          = require('path');
+const bodyParser    = require('body-parser');
+const urlShortener  = require('node-url-shortener');
 
 
-const port        = 3000;
+const port          = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded());
