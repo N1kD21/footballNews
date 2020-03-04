9c04f1425a242aa37ddf958eccafc406066974bc
Nikola <nikoladmit21@gmail.com>
2020-03-04 11:18:22 +0200

    bot telegram 1.0.5

--- app.js
+++ app.js
@@ -40,43 +40,10 @@ const botTelegram = new TelegramBot(token, {polling: true});
 botTelegram.onText(/(.+)/, async (msg, match) => {
   const chatId          = msg.chat.id;
   const resp            = match[1]; // the captured "whatever"
-
-  var questions = [
-    {
-      title:'Сколько параметров можно передать функции ?',
-      buttons: [
-          [{ text: 'Ровно столько, сколько указано в определении функции.', callback_data: '0_1' }],
-          [{ text: 'Сколько указано в определении функции или меньше.', callback_data: '0_2' }],
-          [{ text: 'Сколько указано в определении функции или больше.', callback_data: '0_3' }],
-          [{ text: 'Любое количество.', callback_data: '0_4' }]
-        ],
-      right_answer: 4
-    },
-    {
-      title:'Чему равна переменная name?\nvar name = "пупкин".replace("п", "д")',
-      buttons: [
-          [{ text: 'дудкин', callback_data: '1_1' }],
-          [{ text: 'дупкин', callback_data: '1_2' }],
-          [{ text: 'пупкин', callback_data: '1_3' }],
-          [{ text: 'ляпкин-тяпкин', callback_data: '1_4' }]
-        ],
-      right_answer: 2
-    },
-    {
-      title:'Чему равно 0 || "" || 2 || true ?',
-      buttons: [
-          [{ text: '0', callback_data: '2_1' }],
-          [{ text: '""', callback_data: '2_2' }],
-          [{ text: '2', callback_data: '2_3' }],
-          [{ text: 'true', callback_data: '2_4' }]
-        ],
-      right_answer: 3
-    },
-  ];
-
+  
   let otvetAllSportsAPI = await zaprosFootball();
   for (var indexLeague = 0; indexLeague < otvetAllSportsAPI.length; indexLeague++) {
-    botTelegram.sendMessage(chatId, `${otvetAllSportsAPI[indexLeague].name} is ${otvetAllSportsAPI[indexLeague].plan} League`, questions);
+    botTelegram.sendMessage(chatId, `${otvetAllSportsAPI[indexLeague].name} is ${otvetAllSportsAPI[indexLeague].plan} League`);
   }
 });
 
