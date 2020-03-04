60a13d647cf15ff12c15ecd801747b14602e5a88
Nikola <nikoladmit21@gmail.com>
2020-03-04 11:14:31 +0200

    bot telegram 1.0.4

--- app.js
+++ app.js
@@ -41,19 +41,42 @@ botTelegram.onText(/(.+)/, async (msg, match) => {
   const chatId          = msg.chat.id;
   const resp            = match[1]; // the captured "whatever"
 
-  var options = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [{ text: 'Кнопка 1', callback_data: '1' }],
-      [{ text: 'Кнопка 2', callback_data: 'data 2' }],
-      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
-    ]
-  })
-};
+  var questions = [
+    {
+      title:'Сколько параметров можно передать функции ?',
+      buttons: [
+          [{ text: 'Ровно столько, сколько указано в определении функции.', callback_data: '0_1' }],
+          [{ text: 'Сколько указано в определении функции или меньше.', callback_data: '0_2' }],
+          [{ text: 'Сколько указано в определении функции или больше.', callback_data: '0_3' }],
+          [{ text: 'Любое количество.', callback_data: '0_4' }]
+        ],
+      right_answer: 4
+    },
+    {
+      title:'Чему равна переменная name?\nvar name = "пупкин".replace("п", "д")',
+      buttons: [
+          [{ text: 'дудкин', callback_data: '1_1' }],
+          [{ text: 'дупкин', callback_data: '1_2' }],
+          [{ text: 'пупкин', callback_data: '1_3' }],
+          [{ text: 'ляпкин-тяпкин', callback_data: '1_4' }]
+        ],
+      right_answer: 2
+    },
+    {
+      title:'Чему равно 0 || "" || 2 || true ?',
+      buttons: [
+          [{ text: '0', callback_data: '2_1' }],
+          [{ text: '""', callback_data: '2_2' }],
+          [{ text: '2', callback_data: '2_3' }],
+          [{ text: 'true', callback_data: '2_4' }]
+        ],
+      right_answer: 3
+    },
+  ];
 
   let otvetAllSportsAPI = await zaprosFootball();
   for (var indexLeague = 0; indexLeague < otvetAllSportsAPI.length; indexLeague++) {
-    botTelegram.sendMessage(chatId, `${otvetAllSportsAPI[indexLeague].name} is ${otvetAllSportsAPI[indexLeague].plan} League`, options);
+    botTelegram.sendMessage(chatId, `${otvetAllSportsAPI[indexLeague].name} is ${otvetAllSportsAPI[indexLeague].plan} League`, questions);
   }
 });
 
