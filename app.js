b4f71c666b8825de08d6762a9b68f8c30ca44bf2
Nikola <nikoladmit21@gmail.com>
2020-03-10 09:07:17 +0200

    bot telegram 1.2.6

--- app.js
+++ app.js
@@ -35,8 +35,9 @@ app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
 
 
 //------Telegram
-//
-//
+const token             = '716536032:AAF679qSXFEjD3swXRKINrdgUYfoAysOLpc';
+const chatIdChanelNews  = '-1001382295148';
+
 const botTelegram = new TelegramBot(token, {polling: true});
 
 
@@ -52,18 +53,16 @@ botTelegram.onText(/(.+)/, async (msg, match) => {
   //google news api
   let otvetGoogleNewsAPI = await zaprosFootballNews();
   otvetGoogleNewsAPI.forEach(async(itemArticle) => {
-//    await sayPhoto(chatId, itemArticle.immageUrl)
     await sayMessage(chatId, `${itemArticle.immageUrl}\n${itemArticle.zagolovok}\n${itemArticle.author}\n${itemArticle.nameResourse}\n${itemArticle.dataPublished}`);
   });
-
-  setInterval(async () => {
-    let otvetGoogleNewsApiInteval = await zaprosFootballNews();
-    otvetGoogleNewsApiInteval.forEach(async(itemArticle) => {
-      await sayMessage(chatIdChanelNews, `${itemArticle.immageUrl}\n${itemArticle.zagolovok}\n${itemArticle.author}\n${itemArticle.nameResourse}\n${itemArticle.dataPublished}`);
-    });
-  }, 7200000);
-
 });
+
+setInterval(async () => {
+  let otvetGoogleNewsApiInteval = await zaprosFootballNews();
+  otvetGoogleNewsApiInteval.forEach(async(itemArticle) => {
+    await sayMessage(chatIdChanelNews, `${itemArticle.immageUrl}\n${itemArticle.zagolovok}\n${itemArticle.author}\n${itemArticle.nameResourse}\n${itemArticle.dataPublished}`);
+  });
+}, 7200000);
 
 async function sayPhoto(chatIdSay, urlPhoto) {
   botTelegram.sendPhoto(chatIdSay, urlPhoto);
