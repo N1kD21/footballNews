6e05f236318ac85fd2adfd090bf98d59e6faf12b
NikolaDmit <nikoladmit21@gmail.com>
2023-01-05 16:25:52 +0200

    new test jest

--- tests/sportUA.test.js
+++ tests/sportUA.test.js
@@ -1,14 +1,21 @@
 'use strict';
 
-const zapitDo24UA = require('../lib/sportUA.js');
+const zapit = require('../lib/sportUA.js');
 
-(async() => {
-    const res = await zapitDo24UA({
+
+test('the data is peanut butter', async () => {
+    await expect(zapit({
         source: { 
             name: "Valera Loh"
         },
         url: 'https://sport.ua/uk/news/609435-antonio-konte-eto-bezumie-tottenhemu-nuzhno-pokupat-igrokov-po-50-70-mln'
-    });
-    console.log('6. res - >', res);
-})();
-
+    })).resolves.toStrictEqual({
+        nameResourse: "Valera Loh",
+        author: undefined,
+        zagolovok: "Антоніо КОНТЕ: Це божевілля. Тоттенхему треба купувати гравців по 50-70 млн \n Тренер вважає, що від команди забагато чекають",
+        linkArticle: 'https://sport.ua/uk/news/609435-antonio-konte-eto-bezumie-tottenhemu-nuzhno-pokupat-igrokov-po-50-70-mln',
+        immageUrl: 'https://pic.sport.ua/images/news/0/15/47/orig_609435.jpg',
+        dataPublished: undefined
+      });
+});
+  
